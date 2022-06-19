6fdc7793ee85d1e209d881864d7b2d87c50a5f33
ds <dannysilence@homelabs.company>
2022-06-19 22:06:19 +0300

    base test add

--- cypress/e2e/sample.cy.ts
+++ cypress/e2e/sample.cy.ts
@@ -1,5 +1,7 @@
 describe('Sample Feature', function() {
+
     context(' in positive cases', { env: { allowedAt: [''] }}, function() {
+
         it(' should simply work', { env: { testKey: 'PLAT-12345' }}, function() {
             cy.log('Hello, World');
         });
@@ -7,11 +9,14 @@ describe('Sample Feature', function() {
         it(' should always simply work!', { env: { testKey: 'PLAT-12346' }}, function() {
             cy.log('Bye, World');
         });
+
     }); 
 
     context(' talking about Cypress object itself', { env: { allowedAt: ['any'] }}, function() {
+
         it(' should allow to use instanceOf chains', { env: { testKey: 'PLAT-1234' }}, function() {
             expect(Cypress.version).to.be.a('string');
         });
+        
     });
 });
